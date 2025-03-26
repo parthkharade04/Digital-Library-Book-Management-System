bbe837c6cdaee55e0af6e2d3ea814b843ff18053
parthkharade04 <parthnk04@gmail.com>
2025-03-26 23:18:38 +0530

    Fixed main.py issues

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from src.routes.book_routes import router
 
@@ -6,5 +8,5 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))  # Get port from environment
+    uvicorn.run(app, host="0.0.0.0", port=port)
